2fa53ded160a0988ae165b2f1e6b18d25ff07b9f
SergeyKoavalev <dip.deep@mail.ru>
2021-12-26 00:34:06 +0300

    Added method canCut()

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -11,11 +11,19 @@ public abstract class ChessPiece {
 
     public abstract String getColor();
 
-    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+    public abstract boolean canMoveToPosition(ChessBoard cb, int line, int column, int toLine, int toColumn);
 
     public abstract String getSymbol();
 
     public boolean isOnTheField(int line, int column) {
         return line >= 0 && line <= 7 && column >= 0 && column <= 7;
+    }
+
+    public boolean canCut(ChessBoard cb, int toLine, int toColumn) {
+        if (cb.board[toLine][toColumn] != null) {
+            return cb.board[toLine][toColumn].getColor().equals(color.equals(WHITE) ? BLACK : WHITE);
+        } else {
+            return true;
+        }
     }
 }
