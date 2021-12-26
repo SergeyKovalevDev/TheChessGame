26e8d742d1976a1d585fc81d2dca5a3691dc030a
SergeyKoavalev <dip.deep@mail.ru>
2021-12-26 22:53:20 +0300

    Now the king can move and cut other pieces

--- src/King.java
+++ src/King.java
@@ -12,10 +12,14 @@ public class King extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard cb, int line, int column, int toLine, int toColumn) {
-        return ((line - toLine) == 0 && Math.abs(column - toColumn) == 1 ||
-                (column - toColumn) == 0 && Math.abs(line - toLine) == 1 ||
-                Math.abs(line - toLine) == Math.abs(column - toColumn) && Math.abs(line - toLine) == 1) &&
-                isOnTheField(toLine, toColumn);
+        if (isOnTheField(toLine, toColumn)) {
+            return ((line - toLine) == 0 && Math.abs(column - toColumn) == 1 ||
+                    (column - toColumn) == 0 && Math.abs(line - toLine) == 1 ||
+                    Math.abs(line - toLine) == Math.abs(column - toColumn) && Math.abs(line - toLine) == 1) &&
+                    canMoveOrCut(cb, toLine, toColumn);
+        } else {
+            return false;
+        }
     }
 
     @Override
