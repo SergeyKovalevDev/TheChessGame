dd91c9b180e8306d1c7c4921c5d2aa55e53999be
SergeyKoavalev <dip.deep@mail.ru>
2021-12-27 23:15:19 +0300

    Now the queen can move and cut other pieces

--- src/Queen.java
+++ src/Queen.java
@@ -12,10 +12,17 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard cb, int line, int column, int toLine, int toColumn) {
-        if (isOnTheField(toLine, toColumn)) {
-            return ((line - toLine) == 0 && Math.abs(column - toColumn) > 0 ||
-                    (column - toColumn) == 0 && Math.abs(line - toLine) > 0 ||
-                    Math.abs(line - toLine) == Math.abs(column - toColumn) && Math.abs(line - toLine) > 0);
+        if (isOnTheField(toLine, toColumn) &&
+                ((Math.abs(line - toLine) == Math.abs(column - toColumn) && Math.abs(line - toLine) > 0 || // Ограничиваем движение только по диагонали или
+                        (line - toLine) == 0 && Math.abs(column - toColumn) > 0 ||                         // только вдоль линии или
+                        (column - toColumn) == 0 && Math.abs(line - toLine) > 0))) {                       // только вдоль столбца
+            int lineDir = Integer.compare(toLine, line);
+            int columnDir = Integer.compare (toColumn, column);
+            boolean retVal = true;
+            for (int i = 1; i < Math.max(Math.abs(toLine - line), Math.abs(toColumn - column)); i++) {
+                retVal &= cb.board[line + i * lineDir][column + i  * columnDir] == null;
+            }
+            return retVal && canMoveOrCut(cb, toLine, toColumn);
         } else {
             return false;
         }
