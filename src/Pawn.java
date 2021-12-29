d48123d14fb50b72d8fbf552ec2aeece49ecccc4
Sergey Kovalev <dip.deep@mail.ru>
2021-12-29 14:40:14 +0300

    Changed method canMoveToPosition()

--- src/Pawn.java
+++ src/Pawn.java
@@ -18,20 +18,15 @@ public class Pawn extends ChessPiece {
                     return cb.board[toLine][toColumn] == null;
                 } else if ((toLine - line) == (color.equals(WHITE) ? 2 : -2) && line == (color.equals(WHITE) ? 1 : 6)) {
                     return cb.board[toLine + (color.equals(WHITE) ? - 1 : 1)][toColumn] == null && cb.board[toLine][toColumn] == null;
-                }
+                } else return false;
             } else if (Math.abs(column - toColumn) == 1 && (toLine - line) == (color.equals(WHITE) ? 1 : -1)) {
-                return canMoveOrCut(cb, toLine, toColumn);
-            }
-        }
-        return false;
-    }
-
-    @Override
-    // Метод переопределен т.к. пешка не может ходить по диагонали без рубки и рубить короля
-    public boolean canMoveOrCut(ChessBoard cb, int toLine, int toColumn) {
-        if (cb.board[toLine][toColumn] != null) {
-            return cb.board[toLine][toColumn].getColor().equals(color.equals(WHITE) ? BLACK : WHITE) &&
-                    !cb.board[toLine][toColumn].getSymbol().equals(King.SYMBOL);
+                if (cb.board[toLine][toColumn] != null) {
+                    return cb.board[toLine][toColumn].getColor().equals(color.equals(WHITE) ? BLACK : WHITE) &&
+                            !cb.board[toLine][toColumn].getSymbol().equals(King.SYMBOL);
+                } else {
+                    return false;
+                }
+            } else return false;
         } else {
             return false;
         }
