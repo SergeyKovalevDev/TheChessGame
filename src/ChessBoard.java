89aff48755536672e9576a773076ffb5b092ab67
SergeyKoavalev <dip.deep@mail.ru>
2021-12-31 08:57:18 +0300

    Added checking for the presence of a figure in a position [startLine][startColumn] in method moveToPosition()

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -11,7 +11,7 @@ public class ChessBoard {
     }
 
     public boolean moveToPosition(int startLine, int startColumn, int endLine, int endColumn) {
-        if (checkPos(startLine) && checkPos(startColumn)) {
+        if (checkPos(startLine) && checkPos(startColumn) && board[startLine][startColumn] != null) {
             if (!nowPlayer.equals(board[startLine][startColumn].getColor())) return false;
             if (board[startLine][startColumn].canMoveToPosition(this, startLine, startColumn, endLine, endColumn)) {
                 board[endLine][endColumn] = board[startLine][startColumn]; // if piece can move, we moved a piece
