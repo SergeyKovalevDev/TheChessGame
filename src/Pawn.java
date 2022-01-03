7bf99f7aec2f998fc5ec1a960a98ce1dc5790695
SergeyKoavalev <dip.deep@mail.ru>
2022-01-03 22:58:23 +0300

    Added restrictions on pawn movement

--- src/Pawn.java
+++ src/Pawn.java
@@ -19,14 +19,15 @@ public class Pawn extends ChessPiece {
     public boolean canMoveToPosition(ChessBoard cb, int line, int column, int toLine, int toColumn) {
         if (isOnTheField(toLine, toColumn)) {
             if ((column - toColumn) == 0) {
-                if ((toLine - line) == (color.equals(WHITE) ? 1 : -1)) {
+                if ((toLine - line) == (color.equals(WHITE) ? 1 : -1) && ((color.equals(WHITE) ? line > 0 : line < 7))) {
                     return cb.board[toLine][toColumn] == null;
                 } else if ((toLine - line) == (color.equals(WHITE) ? 2 : -2) && line == (color.equals(WHITE) ? 1 : 6)) {
                     return cb.board[toLine + (color.equals(WHITE) ? - 1 : 1)][toColumn] == null && cb.board[toLine][toColumn] == null;
                 } else {
                     return false;
                 }
-            } else if (Math.abs(column - toColumn) == 1 && (toLine - line) == (color.equals(WHITE) ? 1 : -1)) {
+            } else if (Math.abs(column - toColumn) == 1 && (toLine - line) == (color.equals(WHITE) ? 1 : -1) &&
+                    ((color.equals(WHITE) ? line > 0 : line < 7))) {
                 if (cb.board[toLine][toColumn] != null) {
                     return cb.board[toLine][toColumn].getColor().equals(color.equals(WHITE) ? BLACK : WHITE) &&
                             !cb.board[toLine][toColumn].getSymbol().equals(King.SYMBOL);
